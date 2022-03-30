fb89569139950385932cf039a2c33a19b8abd6c5
Nathan Nichols <nwnichols02@gmail.com>
2022-03-30 15:52:08 -0600

    working delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
@@ -14,6 +14,8 @@ const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
+  const {push} = useHistory();
+
   useEffect(()=>{
     axios.get('http://localhost:9000/api/movies')
       .then(res => {
@@ -25,6 +27,16 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    axios.delete(`http://localhost:9000/api/movies/${id}`, )
+      .then(res => {
+        setMovies(movies.filter(mov => {
+          return mov.id !== id
+      }))
+        push(`/movies`)
+      })
+      .catch(err => {
+        debugger
+      })
   }
 
   const addToFavorites = (movie) => {
@@ -48,7 +60,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
